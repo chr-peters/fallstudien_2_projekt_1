ac30e807f167888b3381f3cfc6574a8a7526618d
laura-kampmann <74373368+laura-kampmann@users.noreply.github.com>
2020-11-26 21:57:32 +0100

    Update lstm.R

--- scripts/lstm.R
+++ scripts/lstm.R
@@ -1,14 +1,6 @@
 library(tidyverse)
 library(tibble)
-library(lubridate)
-library(recipes)
-library(hablar)
-library(timetk)
-library(rsample)
-library(tibbletime)
 library(keras)
-#library(mapfuser)
-library(anytime)
 library(abind)
 
 setwd("~/fallstudien_2_projekt_1/data_raw/campus")
@@ -22,31 +14,37 @@ for (provider in providers){
     mutate(provider=provider) %>% rbind(ul_data) %>% na.omit()
 }
 
+# Scale data
+features <- c("throughput_mbits", "payload_mb", "f_mhz")
+train_size <- ceiling(nrow(ul_data_cropped)*0.9)
+test_size <- floor(nrow(ul_data_cropped)*0.10)
+# Divide data into test and train
+ul_train <- ul_data[1:train_size, features]
+ul_test <- ul_data[(train_size+1):(train_size+test_size), features]
+ul_train_scaled <- data.frame(scale(ul_train))
+ul_test_scaled <- scale(ul_test, center = attr(ul_train_scaled, "scaled:center"), 
+                        scale = attr(ul_train_scaled, "scaled:scale"))
 
-# create variable day to include different drives
-ul_data$day <- ul_data$timestamp_ms %>% anytime() %>% day
+tsteps_in <- 7
+tsteps_out <- 72
 
-ul_data_cropped <- ul_data[, c("throughput_mbits", "payload_mb", "")]
-ul_data_cropped[c("throughput_mbits", "payload_mb")] <- 
-  scale(ul_data_cropped[c("throughput_mbits", "payload_mb")])
-ul_data_cropped <- dummy_cols(ul_data_cropped)
-t_steps <- 7
-steps_out <- 2
-
-X <- array(dim=c(0,t_steps,ncol(ul_data_cropped)-1))
-y <- array(dim=c(0, 2))
-for (i in 1:(194+t_steps)){
-  end_in <- i+t_steps-1
-  end_out <- i+steps_out-1
-  if (end_out > (194+t_steps)) break
+X_train <- array(dim=c(0,tsteps_in,ncol(ul_train_scaled)-1)) # minus throughput
+X_test <- array(dim=c(0,tsteps_in,ncol(ul_data_cropped)-1)) # minus throughput
+y_train <- array(dim=c(0, 72))
+y_test <- array(dim=c(0, 72))
+for (i in 1:nrow(ul_train_scaled)){
+  end_in <- i+tsteps_in-1
+  end_out <- i+tsteps_out-1
+  if (end_out > (nrow(ul_train_scaled)-tsteps_in)) break
   seq_x <- array(
-    ul_data_cropped[i:(end_in), 
-            names(ul_data_cropped) != "throughput_mbits" & names(ul_data_cropped) != "provider"], 
-    dim=c(1, t_steps, ncol(ul_data_cropped)-1))
-  seq_y <- array(ul_data_cropped[i:end_out, "throughput_mbits"], 
-                 dim=c(1, steps_out, 1))
-  X <- abind(X, seq_x, along = 1)
-  y <- abind(y, seq_y, along = 1)
+    ul_train_scaled[i:end_in, 
+            names(ul_train_scaled) != "throughput_mbits"], 
+    dim=c(1, tsteps_in, ncol(ul_train_scaled)-1))
+  seq_y <- array(
+    ul_train_scaled[i:end_out, "throughput_mbits"], 
+    dim=c(1, tsteps_out))
+  X_test <- abind(X, seq_x, along = 1)
+  y_test <- abind(y, seq_y, along = 1)
 }
 
 
@@ -58,13 +56,13 @@ model <- keras_model_sequential()
 
 model %>%
   layer_lstm(units            = 50, 
-             activation       = "relu", 
-             input_shape      = c(t_steps, dim(X)[3]),
+             #activation       = "relu", 
+             input_shape      = c(tsteps_in, dim(X)[3]),
              batch_size       = batch_size,
              return_sequences = TRUE, 
              stateful         = TRUE) %>% 
   layer_lstm(units            = 50, 
-             activation       = "relu",
+             #activation       = "relu",
              return_sequences = FALSE, 
              stateful         = TRUE) %>% 
   layer_dense(units = 2)
@@ -74,7 +72,7 @@ model %>%
 
 # 5.1.7 Fitting LSTM
 for (i in 1:epochs) {
-  model %>% fit(x          = X[1:198, , ], 
+  model %>% fit(x          = X[1:, , ], 
                 y          = y[1:198, ], 
                 batch_size = batch_size,
                 epochs     = 1, 
@@ -88,10 +86,10 @@ for (i in 1:epochs) {
 
 # Make Predictions
 pred_out <- model %>% 
-  predict(X[181:200, , ], batch_size) #%>%
+  predict(X[199:200, , ], batch_size) #%>%
   #.[,1] 
 pred_out
-y[181:200]
+y[199:200,]
 
 ggplot(data.frame(actual=y[181:200], predict=pred_out), aes(x=actual, y=predict)) + 
          geom_point()
