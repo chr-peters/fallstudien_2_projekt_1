af1df1bd110599f1c09fc3782540c26906fd3433
laura-kampmann <74373368+laura-kampmann@users.noreply.github.com>
2020-11-27 14:52:25 +0100

    Update lstm.R

--- scripts/lstm.R
+++ scripts/lstm.R
@@ -18,8 +18,8 @@ for (provider in providers){
 
 # Scale data
 features <- c("throughput_mbits", "payload_mb")
-train_size <- 500 #ceiling(nrow(ul_data)*0.05)
-test_size <- 2 # floor(nrow(ul_data)*0.02)
+train_size <- 500 
+test_size <- 20
 
 # Divide data into test and train
 ul_train <- ul_data[1:train_size, features]
@@ -28,8 +28,12 @@ ul_train_scaled <- scale(ul_train)
 ul_test_scaled <- scale(ul_test, center = attr(ul_train_scaled, "scaled:center"), 
                         scale = attr(ul_train_scaled, "scaled:scale"))
 
+# Save transformation
+center_history <- attr(ul_train_scaled, "scaled:center")
+scale_history <- attr(ul_train_scaled, "scaled:scale")
+
 # include lag
-nlag <- 2
+nlag <- 2 # nlag >= test_size
 ul_train_scaled <- cbind(
   ul_train_scaled, 
   "throughput_lag" = shift(ul_train_scaled[,"throughput_mbits"], nlag)
@@ -47,9 +51,8 @@ ul_train_scaled <- na.omit(ul_train_scaled)
 # - cols: timesteps (tsteps_in)
 # - height: number of features
 
-tsteps_in <- 2
-tsteps_out <- 2 # first tsteps_in-1 will not be predicted 
-                # -> nrow(testdata) >= (tsteps_in-1)+tsteps_out
+tsteps_in <- 3
+tsteps_out <- 2
 
 create_matrices <- function (data, tsteps_in, tsteps_out, y_col = "throughput_mbits", 
                       n_features = ncol(data)-1){
@@ -125,14 +128,11 @@ for (i in 1:epochs) {
 }
 
 # Make Predictions
-pred_out <- model %>% 
-  predict(test$X, batch_size) #%>%
-  #.[,1] 
-pred_out[1,]
+pred_out <- model %>% predict(test$X, batch_size)
+
+pred_out
 ul_test_scaled[,"throughput_mbits"]
 
-ggplot(
-  data = data.frame(actual=ul_test_scaled[, "throughput_mbits"], predict=pred_out[1,]), 
-  aes(x = actual, y = predict)
-  ) + geom_point()
+
+
 
