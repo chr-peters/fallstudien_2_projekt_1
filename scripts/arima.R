1dfcbdfadf551105c26e021846fb7abb8e1981de
laura-kampmann <74373368+laura-kampmann@users.noreply.github.com>
2020-12-02 00:19:09 +0100

    Update arima.R

--- scripts/arima.R
+++ scripts/arima.R
@@ -1,5 +1,11 @@
 library(forecast)
 library(fastDummies)
+library(dplyr)
+library(anytime)
+library(ggplot2)
+library(Metrics)
+library(corrplot)
+
 
 setwd("~/fallstudien_2_projekt_1/data_raw/campus")
 ul_filenames <- dir("~/fallstudien_2_projekt_1/data_raw/campus") %>% 
@@ -12,22 +18,50 @@ for (provider in providers){
     mutate(provider=provider) %>% rbind(ul_data) %>% na.omit()
 }
 
+# Sort Data
+ul_data <- ul_data[order(ul_data$timestamp), ]
+
+# Data
+ul_data$timestamp <- ul_data$timestamp_ms %>% anytime()
+ul_data$index <- 1:nrow(ul_data)
+
+# Sort Data
+ul_data <- ul_data[order(ul_data$timestamp), ]
+
+# Graphs
+ggplot(
+  ul_data[, c("timestamp", "throughput_mbits")], 
+  aes(x=timestamp, y=throughput_mbits)
+  ) + 
+  geom_line()
+
+ggplot(
+  ul_data[, c("index", "throughput_mbits")], 
+  aes(x=index, y=throughput_mbits)
+) + 
+  geom_line()
+
+
+acf(ul_data$throughput_mbits)
+pacf(ul_data$throughput_mbits)
+
 # Select features
-ul_data$number <- 1:nrow(ul_data)
 ul_data$day <- ul_data$timestamp_ms %>% anytime() %>% format("%d") %>% as.integer()
 ul_data$hour <- ul_data$timestamp_ms %>% anytime() %>% format("%H") %>% as.integer()
 
 features <- c("throughput_mbits", "payload_mb", "f_mhz", 
-              "rsrp_dbm", "rsrq_db", "pci", "ta", "id", "velocity_mps", 
-              "provider", "day", "hour")
+              "rsrp_dbm", "rsrq_db", "ta")
 numeric_features <- features[as.vector(unlist(lapply(ul_data[, features], is.numeric)))]
 ycol <- "throughput_mbits"
 
-X <- ul_data[, features] %>% dummy_cols(
-  select_columns = c("provider"), 
-  remove_selected_columns = TRUE, 
-  remove_first_dummy = TRUE
-  )
+cormat <- cor(ul_data[, numeric_features])
+corrplot(cormat, method="color")
+
+# TODO: Check if chr columns in data
+X <- ul_data[, features] #%>% dummy_cols(
+  #remove_selected_columns = TRUE, 
+  #remove_first_dummy = TRUE
+  #)
 
 
 # Scale Data
@@ -61,10 +95,11 @@ for (col in colnames(Xtrain)){
 }
 
 # Model
-model <- auto.arima(ytrain, xreg = Xtrain, lambda = 0)
+model <- auto.arima(ytrain, xreg = Xtrain, lambda = "auto")
+arimaorder(model)
 
 # Prediction
-prediction <- forecast(model, xreg = Xtest, lambda = 0)
+prediction <- forecast(model, xreg = Xtest)
 
 # Reverse scaling
 pred_values <- as.numeric(prediction$mean * scale_history["throughput_mbits"] + center_history["throughput_mbits"])
@@ -82,4 +117,7 @@ plot_data <- rbind(data.frame(value=pred_values, type="predict", index=1:length(
                   data.frame(value=actual, type="actual", index=1:length(actual)))
 
 ggplot(plot_data, aes(x=index, y=value, color=type)) + geom_line()
+
+rmse(actual, pred_values)
+mae(actual, pred_values)  
 
